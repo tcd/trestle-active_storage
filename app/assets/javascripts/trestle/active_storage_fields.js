ac3adafe38a4281ebd9d2648c8471cd720039a6a
Clay Dunston <claydunston@gmail.com>
2021-12-03 17:27:45 -0600

    Testing JS refactor

--- app/assets/javascripts/trestle/active_storage_fields.js
+++ app/assets/javascripts/trestle/active_storage_fields.js
@@ -1,25 +1,29 @@
-Trestle.ready(function() {
-  function init() {
-    var fields = document.querySelectorAll(".active-storage__field");
+TRESTLE_ACTIVE_STORAGE = {}
+
+TRESTLE_ACTIVE_STORAGE.init = () => {
+    let fields = document.querySelectorAll(".active-storage__field")
 
     for (var i = 0; i < fields.length; i++) {
-      attachEvents(fields[i]);
+        attachEvents(fields[i])
     }
-  }
+}
 
-  function attachEvents(field) {
-    var progressEl = field.parentNode.querySelectorAll(".progress")[0];
-    var progressBarEl = field.parentNode.querySelectorAll(".progress-bar")[0];
+TRESTLE_ACTIVE_STORAGE.attachEvents = (field) => {
+  let progressEl    = field.parentNode.querySelectorAll(".progress")[0]
+  let progressBarEl = field.parentNode.querySelectorAll(".progress-bar")[0]
 
-    field.addEventListener("direct-upload:start", function(event) {
-      progressEl.style = "display: block";
-    });
+  field.addEventListener("direct-upload:start", (event) => {
+    progressEl.style = "display: block"
+  })
 
-    field.addEventListener("direct-upload:progress", function(event) {
-      var detail = event.detail;
-      progressBarEl.style = "width: " + detail.progress + "%";
-    });
-  }
+  field.addEventListener("direct-upload:progress", (event) => {
+    let detail = event.detail
+    progressBarEl.style = "width: " + detail.progress + "%"
+  })
+}
 
-  init();
-});
+Trestle.ready(() => {
+    console.log("Custom 'trestle-active_storage' loaded")
+    window.TRESTLE_ACTIVE_STORAGE = TRESTLE_ACTIVE_STORAGE
+    TRESTLE_ACTIVE_STORAGE.init()
+})
